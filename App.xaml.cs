3ccc5510e9fcbad1976ffb7e7059d96b02ae9144
ZoliKonya <zoltan.konya@yahoo.com>
2023-12-01 16:58:50 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Konya_Zoltan_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
