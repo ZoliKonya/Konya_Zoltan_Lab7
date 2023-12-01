e15aa3e7d1f06e00e293ff5368dddfe352b7eb43
ZoliKonya <zoltan.konya@yahoo.com>
2023-12-01 18:51:11 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Konya_Zoltan_Lab7
+﻿using System;
+using Konya_Zoltan_Lab7.Data;
+using System.IO;
+
+namespace Konya_Zoltan_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
